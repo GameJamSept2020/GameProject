636fb2aa4cffa88980415fa524a85e383b980fe4
Kyle <kylemcalinden@hotmail.com>
2020-09-09 20:58:12 +0100

    character movement

---
+++ GameJamProject/Assets/Scripts/ScoreSystemController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreSystemController : MonoBehaviour
+{
+    public int scoreIncreaseNumber;
+    void Update()
+    {
+        
+    }
+}
